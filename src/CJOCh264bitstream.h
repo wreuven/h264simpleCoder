5af2733931b8dcb1b1c077cfc3d1fde54b07790f
wreuven <wreuven@users.noreply.github.com>
2021-04-06 11:33:20 +0300

    Update CJOCh264bitstream.h

--- src/CJOCh264bitstream.h
+++ src/CJOCh264bitstream.h
@@ -13,107 +13,100 @@
 #include <string.h>
 #include <math.h>
 
-//! h264 bitstream class
-/*!
- It is used to create the h264 bit oriented stream, it contains different functions that helps you to create the h264 compliant stream (bit oriented, exp golomb coder)
- */
-class CJOCh264bitstream
-{
-#define BUFFER_SIZE_BITS 24			/*! Buffer size in bits used for emulation prevention */
+#define BUFFER_SIZE_BITS 24		/*! Buffer size in bits used for emulation prevention */
 #define BUFFER_SIZE_BYTES (24/8)	/*! Buffer size in bytes used for emulation prevention */
 
 #define H264_EMULATION_PREVENTION_BYTE 0x03		/*! Emulation prevention byte */
 
-private:
 
-	/*! Buffer  */
-	unsigned char m_buffer[BUFFER_SIZE_BITS];
+//! h264 bitstream class
+/*!
+ It is used to create the h264 bit oriented stream, it contains different functions that helps you to create the h264 compliant stream (bit oriented, exp golomb coder)
+ */
 
-	/*! Bit buffer index  */
-	unsigned int m_nLastbitinbuffer;
+/*! Buffer  */
+unsigned char m_buffer[BUFFER_SIZE_BITS];
 
-	/*! Starting byte indicator  */
-	unsigned int m_nStartingbyte;
+/*! Bit buffer index  */
+unsigned int m_nLastbitinbuffer;
 
-	/*! Pointer to output file */
-	FILE *m_pOutFile;
+/*! Starting byte indicator  */
+unsigned int m_nStartingbyte;
 
-	//! Clears the buffer
-	void clearbuffer();
+/*! Pointer to output file */
+FILE *m_pOutFile;
 
-	//! Returns the nNumbit value (1 or 0) of lval
-	/*!
-	 	 \param lval number to extract the nNumbit value
-	 	 \param nNumbit Bit position that we want to know if its 1 or 0 (from 0 to 63)
-	 	 \return bit value (1 or 0)
-	 */
-	static int getbitnum (unsigned long lval, int nNumbit);
+//! Clears the buffer
+void clearbuffer();
 
-	//! Adds 1 bit to the end of h264 bitstream
-	/*!
-		 \param nVal bit to add at the end of h264 bitstream
-	 */
-	void addbittostream (int nVal);
+//! Returns the nNumbit value (1 or 0) of lval
+/*!
+	 \param lval number to extract the nNumbit value
+	 \param nNumbit Bit position that we want to know if its 1 or 0 (from 0 to 63)
+	 \return bit value (1 or 0)
+ */
+static int getbitnum (unsigned long lval, int nNumbit);
 
-	//! Adds 8 bit to the end of h264 bitstream (it is optimized for byte aligned situations)
-	/*!
-		 \param nVal byte to add at the end of h264 bitstream (from 0 to 255)
-	 */
-	void addbytetostream (int nVal);
+//! Adds 1 bit to the end of h264 bitstream
+/*!
+	 \param nVal bit to add at the end of h264 bitstream
+ */
+void addbittostream (int nVal);
 
-	//! Save all buffer to file
-	/*!
-		 \param bemulationprevention Indicates if it will insert the emulation prevention byte or not (when it is needed)
-	 */
-	void savebufferbyte(bool bemulationprevention = true);
+//! Adds 8 bit to the end of h264 bitstream (it is optimized for byte aligned situations)
+/*!
+	 \param nVal byte to add at the end of h264 bitstream (from 0 to 255)
+ */
+void addbytetostream (int nVal);
 
-public:
-	//! Constructor
-	/*!
-		 \param pOutBinaryFile The output file pointer
-	 */
-	CJOCh264bitstream(FILE *pOutBinaryFile);
+//! Save all buffer to file
+/*!
+	 \param bemulationprevention Indicates if it will insert the emulation prevention byte or not (when it is needed)
+ */
+void savebufferbyte(bool bemulationprevention = true);
 
-	//! Destructor
-	virtual ~CJOCh264bitstream();
+//! Constructor
+/*!
+	 \param pOutBinaryFile The output file pointer
+ */
+CJOCh264bitstream(FILE *pOutBinaryFile);
 
-	//! Add 4 bytes to h264 bistream without taking into acount the emulation prevention. Used to add the NAL header to the h264 bistream
-	/*!
-		 \param nVal The 32b value to add
-		 \param bDoAlign Indicates if the function will insert 0 in order to create a byte aligned stream before adding nVal 4 bytes to stream. If you try to call this function and the stream is not byte aligned an exception will be thrown
-	 */
-	void add4bytesnoemulationprevention (unsigned int nVal, bool bDoAlign = false);
+//! Add 4 bytes to h264 bistream without taking into acount the emulation prevention. Used to add the NAL header to the h264 bistream
+/*!
+	 \param nVal The 32b value to add
+	 \param bDoAlign Indicates if the function will insert 0 in order to create a byte aligned stream before adding nVal 4 bytes to stream. If you try to call this function and the stream is not byte aligned an exception will be thrown
+ */
+void add4bytesnoemulationprevention (unsigned int nVal, bool bDoAlign = false);
 
-	//! Adds nNumbits of lval to the end of h264 bitstream
-	/*!
-		 \param nVal value to add at the end of the h264 stream (only the LAST nNumbits will be added)
-		 \param nNumbits number of bits of lval that will be added to h264 stream (counting from left)
-	 */
-	void addbits (unsigned long lval, int nNumbits);
+//! Adds nNumbits of lval to the end of h264 bitstream
+/*!
+	 \param nVal value to add at the end of the h264 stream (only the LAST nNumbits will be added)
+	 \param nNumbits number of bits of lval that will be added to h264 stream (counting from left)
+ */
+void addbits (unsigned long lval, int nNumbits);
 
-	//! Adds lval to the end of h264 bitstream using exp golomb coding for unsigned values
-	/*!
-		 \param nVal value to add at the end of the h264 stream
-	 */
-	void addexpgolombunsigned (unsigned long lval);
+//! Adds lval to the end of h264 bitstream using exp golomb coding for unsigned values
+/*!
+	 \param nVal value to add at the end of the h264 stream
+ */
+void addexpgolombunsigned (unsigned long lval);
 
-	//! Adds lval to the end of h264 bitstream using exp golomb coding for signed values
-	/*!
-		 \param nVal value to add at the end of the h264 stream
-	 */
-	void addexpgolombsigned (long lval);
+//! Adds lval to the end of h264 bitstream using exp golomb coding for signed values
+/*!
+	 \param nVal value to add at the end of the h264 stream
+ */
+void addexpgolombsigned (long lval);
 
-	//! Adds 0 to the end of h264 bistream in order to leave a byte aligned stream (It will insert seven 0 maximum)
-	void dobytealign();
+//! Adds 0 to the end of h264 bistream in order to leave a byte aligned stream (It will insert seven 0 maximum)
+void dobytealign();
 
-	//! Adds cByte (8 bits) to the end of h264 bitstream. This function it is optimized in byte aligned streams.
-	/*!
-		 \param cByte value to add at the end of the h264 stream (from 0 to 255)
-	 */
-	void addbyte (unsigned char cByte);
+//! Adds cByte (8 bits) to the end of h264 bitstream. This function it is optimized in byte aligned streams.
+/*!
+	 \param cByte value to add at the end of the h264 stream (from 0 to 255)
+ */
+void addbyte (unsigned char cByte);
 
-	//! Close the h264 stream saving to disk the last remaing bits in buffer
-	void close();
-};
+//! Close the h264 stream saving to disk the last remaing bits in buffer
+void close();
 
 #endif /* CJOCH264BITSTREAM_H_ */
